16ee6aae932172415b29f9d8b9e4c5e96939e1bc
YOUNG HO CHA <ganachoco@linecorp.com>
2021-08-11 11:46:32 +0900

    add descriptions

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,7 +18,15 @@ class MainActivity : AppCompatActivity() {
         val inside: View = layoutInflater.inflate(R.layout.child_inside, child2)
 
         val child1binding = ChildInsideBinding.bind(outside.findViewById(R.id.inside_child))
+        // By https://android.googlesource.com/platform/frameworks/data-binding/+/8fc33019
+        // child1binding.insideChild points
+        // Until AGP 4.2, https://github.com/ganadist/VersionCodeDemo/blob/af8d87c5/app/src/main/res/layout/child_outside.xml#L7
+        // Since AGP 7.0, https://github.com/ganadist/VersionCodeDemo/blob/af8d87c5/app/src/main/res/layout/child_inside.xml#L7
+
         val child2binding = ChildInsideBinding.bind(inside.findViewById(R.id.inside_child))
+        // child2binding.insideChild points
+        // Until AGP 4.2, https://github.com/ganadist/VersionCodeDemo/blob/af8d87c5/app/src/main/res/layout/child_inside.xml#L7
+        // Since AGP 7.0, raise NullPointerException when call bind()
 
         println(child1binding)
         println(child2binding)
