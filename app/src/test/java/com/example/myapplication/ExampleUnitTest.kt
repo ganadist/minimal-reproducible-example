c9ee1f00bed295a29c842e8364c43ffe2a445a0e
YOUNG HO CHA <ganachoco@linecorp.com>
2021-11-02 19:10:16 +0900

    Added png decode test with robolectric 4.5.1

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,17 +1,30 @@
 package com.example.myapplication
 
+import android.graphics.BitmapFactory
+import org.apache.commons.io.IOUtils
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4L, 2 + 2L)
+    fun decode_png() {
+        val istream = ClassLoader.getSystemClassLoader().getResourceAsStream("archlinux-logo.png")
+        val tmp = File.createTempFile("test", "image")
+        val output = BufferedOutputStream(FileOutputStream(tmp))
+
+        IOUtils.copy(istream, output)
+
+        BitmapFactory.decodeFile(tmp.absolutePath)
     }
 }
