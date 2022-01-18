e4307c1d38c6525e34ae6be99625470a9681bd2d
YOUNG HO CHA <ganachoco@linecorp.com>
2022-01-18 12:14:03 +0900

    Added GMS Location test

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,15 +1,28 @@
 package com.example.myapplication
 
+import com.google.android.gms.location.FusedLocationProviderClient
+import junit.framework.TestCase
+import org.junit.Rule
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import org.robolectric.RobolectricTestRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+@RunWith(RobolectricTestRunner::class)
+class ExampleUnitTest : TestCase() {
+    @get:Rule
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @Mock
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4L, 2 + 2L)
