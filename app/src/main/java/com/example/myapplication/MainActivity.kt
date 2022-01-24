9d2e38c8b3091afe8c9e5eb680a928f507cc0d91
YOUNG HO CHA <ganachoco@linecorp.com>
2022-01-24 22:14:40 +0900

    Launch activity at install-time dynamic feature

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +9,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val startupIntent = Intent()
+        startupIntent.component = ComponentName(this, "com.example.dynamicfeature1.TestActivity")
+        startupIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(startupIntent)
     }
 }
