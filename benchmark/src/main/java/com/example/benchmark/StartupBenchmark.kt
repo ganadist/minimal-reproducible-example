e17ded28f97d16b33af0b2e520591a80d438825f
YOUNG HO CHA <ganachoco@linecorp.com>
2022-01-24 21:56:17 +0900

    Add macrobenchmark module

---
+++ benchmark/src/main/java/com/example/benchmark/StartupBenchmark.kt
@@ -0,0 +1,25 @@
+package com.example.benchmark.startup
+
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SampleStartupBenchmark {
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startup() = benchmarkRule.measureRepeated(
+        packageName = "com.example.myapplication",
+        metrics = listOf(StartupTimingMetric()),
+        iterations = 5,
+    ) {
+        startActivityAndWait()
+    }
+}
