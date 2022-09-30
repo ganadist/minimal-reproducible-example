33938f4ef5167547c80abefa3b23c9303bb60805
YOUNG HO CHA <ganachoco@linecorp.com>
2022-10-01 08:33:49 +0900

    Added empty glide modules

---
+++ lib/glide/src/main/java/com/example/mylib/LibraryModule.kt
@@ -0,0 +1,13 @@
+package com.example.mylib
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.LibraryGlideModule
+
+@GlideModule
+class LibraryModule : LibraryGlideModule() {
+
+
+}
