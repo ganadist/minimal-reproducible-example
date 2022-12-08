b1ca3bd6aa9ef8c9be9b9f531c8483b4990fb7e4
YOUNG HO CHA <ganachoco@linecorp.com>
2022-12-08 17:46:06 +0900

    Reference string resource which is added with runtimeOnly configuration

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        // com.myapplication.feature.impl.R is referenced with runtimeOnly
+        @StringRes
+        private val FEATURE_STRING_RESID = com.myapplication.feature.impl.R.string.feature_name
     }
 }
