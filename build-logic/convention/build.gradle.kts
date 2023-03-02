fc4ba53b055b93984654bc5cdf3de3810d9594e9
YOUNG CHA <ganachoco@linecorp.com>
2023-03-02 09:23:47 +0900

    Update toolchains

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -2,11 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
     implementation(libs.android.gradle)
     implementation(libs.kotlin.gradle)
