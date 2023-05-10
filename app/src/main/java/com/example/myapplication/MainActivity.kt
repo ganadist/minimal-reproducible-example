886a241cb34d597ede3b7a9ff7fed0e2515ecfc0
YOUNG HO CHA <ganachoco@linecorp.com>
2023-05-10 21:19:45 +0900

    Print android_id

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myapplication
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val android_id = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        Log.d("MainActivity", "android_id = $android_id")
+
     }
 }
