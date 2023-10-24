1dce284ea2b5590476f7ff448c4b2c6289604b12
YOUNG HO CHA <ganachoco@linecorp.com>
2023-10-25 01:31:08 +0900

    Add baseprofile sub project which was generated from Android Studio

---
+++ baselineprofile/build.gradle.kts
@@ -0,0 +1,61 @@
+import com.android.build.api.dsl.ManagedVirtualDevice
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.test)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidx.baselineprofile)
+}
+
+android {
+    namespace = "com.example.baselineprofile"
+    compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    defaultConfig {
+        minSdk = 28
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    targetProjectPath = ":app"
+
+    flavorDimensions += listOf("default")
+    productFlavors {
+        create("develop") { dimension = "default" }
+        create("beta") { dimension = "default" }
+        create("staging") { dimension = "default" }
+        create("production") { dimension = "default" }
+    }
+
+    testOptions.managedDevices.devices {
+        create<ManagedVirtualDevice>("pixel6Api34") {
+            device = "Pixel 6"
+            apiLevel = 34
+            systemImageSource = "google"
+        }
+    }
+}
+
+// This is the configuration block for the Baseline Profile plugin.
+// You can specify to run the generators on a managed devices or connected devices.
+baselineProfile {
+    managedDevices += "pixel6Api34"
+    useConnectedDevices = false
+}
+
+dependencies {
+    implementation(libs.junit)
+    implementation(libs.espresso.core)
+    implementation(libs.uiautomator)
+    implementation(libs.benchmark.macro.junit4)
+}
