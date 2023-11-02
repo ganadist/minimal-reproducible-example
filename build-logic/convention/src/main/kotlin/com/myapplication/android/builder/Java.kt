836e9a8f36317dc70513de61e2e90ba4073fc90e
YOUNG HO CHA <ganachoco@linecorp.com>
2023-11-02 23:40:04 +0900

    Adjust javac option to allow aidl compilation

--- build-logic/convention/src/main/kotlin/com/myapplication/android/builder/Java.kt
+++ build-logic/convention/src/main/kotlin/com/myapplication/android/builder/Java.kt
@@ -4,7 +4,13 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
 
-private val JVM_OPTIONS = listOf("-Xlint:all", "-Werror")
+private val JVM_VERSION = System.getProperty("java.specification.version")
+private val JVM_OPTIONS = if (JVM_VERSION == "21") {
+    // https://issuetracker.google.com/issues/294422895
+    listOf("-Xlint:all,-this-escape", "-Werror")
+} else {
+    listOf("-Xlint:all", "-Werror")
+}
 
 internal fun Project.configureJava(javaVersion: JavaVersion) {
     tasks.withType(JavaCompile::class.java).configureEach {
