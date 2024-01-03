55770cd78967e60bdec13d8da589c99dc2f21001
YOUNG HO CHA <ganachoco@linecorp.com>
2024-01-03 18:22:08 +0900

    Add sample code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.File
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
+    private val paths: Array<File> = File(".").listFiles()?.also {
+        Arrays.sort(it)
+    } ?: throw RuntimeException("here")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
