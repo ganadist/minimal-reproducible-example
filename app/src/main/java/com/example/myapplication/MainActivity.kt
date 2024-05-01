10eb356c9fd2992bc52a3346c4f4dd49eef0fa06
YOUNG HO CHA <ganachoco@linecorp.com>
2024-05-01 16:19:13 +0900

    Add typealias for deprecated LocalBroadcastManager

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,19 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+@Suppress("DEPRECATION")
+private typealias DeprecatedLocalBroadcastManager =
+    androidx.localbroadcastmanager.content.LocalBroadcastManager
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        @Suppress("TYPEALIAS_EXPANSION_DEPRECATION")
+        DeprecatedLocalBroadcastManager.getInstance(this)
+            .sendBroadcast(android.content.Intent("com.example.myapplication"))
+
     }
 }
