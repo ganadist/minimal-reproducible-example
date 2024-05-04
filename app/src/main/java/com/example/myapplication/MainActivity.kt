4fa2c2267f77cc0ac8b22c6931236a64fa0607a2
YOUNG HO CHA <ganachoco@linecorp.com>
2024-05-04 21:50:21 +0900

    Added RESID_MAP

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // https://issuetracker.google.com/issues/209843426
+    @Suppress("WRONG_ANNOTATION_TARGET")
+    private val RESID_MAP = mapOf<Int, @IdRes Int>(
+        1 to R.id.text_view,
+    )
 }
