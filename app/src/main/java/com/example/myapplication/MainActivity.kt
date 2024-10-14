3d18295eef1e3266b26c155f621c921e01c61cd1
YOUNG HO CHA <ganachoco@linecorp.com>
2024-10-14 15:13:40 +0900

    Preparing to use OssLicense activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button_licenses).setOnClickListener() {
+            val intent = Intent(this, OssLicensesMenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
