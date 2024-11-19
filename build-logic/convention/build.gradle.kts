cae7957475787a0ad1b5df1572b83f7de8e3af8b
YOUNG HO CHA <ganachoco@linecorp.com>
2024-11-19 15:08:01 +0900

    Update toolchains

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -23,10 +23,5 @@ gradlePlugin {
             id = "com.myapplication.android.versions.checker"
             implementationClass = "AndroidVersionCatalogsCheckerPlugin"
         }
-        register("composeK2MigrationPlugin") {
-            id = "com.myapplication.compose.k2.migration"
-            implementationClass = "ComposeK2MigrationPlugin"
-            version = "0.0.1"
-        }
     }
 }
