45be79365e93ff08f4e88d07e7aa47e4d3c704ca
YOUNG HO CHA <ganachoco@linecorp.com>
2025-06-20 00:17:08 +0900

    add data class

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+}
+
+class ColorItem(
+    val name: String,
+    @VisibleForTesting private val color: Int = 0
+) {
+    override fun toString(): String {
+        return name
     }
 }
